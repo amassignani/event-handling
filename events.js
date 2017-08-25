e921b78f7c5dd60cd176d22a6ca93d021a186aee
amassignani <feelsgreat2016@gmail.com>
2017-08-25 15:04:57 +1200

    adding event handling

--- events.js
+++ events.js
@@ -4,48 +4,32 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  // The first example is done for you. This will change the background colour of the first div
-  // when you mouse over it.
   one()
   two()
   three()
   four()
-  // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
-  // First, we have to find the element:
   var one = document.getElementById('one')
-
-  // Next, we add an event listener to it:
   one.addEventListener('mouseenter', makeBlue)
-
-  // Finally, we add one to make the colour white again
   one.addEventListener('mouseleave', makeWhite)
+  one.addEventListener('click', makeYellow)
 }
 
 // CREATE FUNCTION two HERE
 function two () {
-  // First, we have to find the element:
   var two = document.getElementById('two')
-
-  // Next, we add an event listener to it:
   two.addEventListener('mouseenter', makeGreen)
-
-  // Finally, we add one to make the colour white again
   two.addEventListener('mouseleave', makeWhite)
+  two.addEventListener('click', makeYellow)
 }
-
 // CREATE FUNCTION three HERE
 function three () {
-  // First, we have to find the element:
   var three = document.getElementById('three')
-
-  // Next, we add an event listener to it:
   three.addEventListener('mouseenter', makeOrange)
-
-  // Finally, we add one to make the colour white again
   three.addEventListener('mouseleave', makeWhite)
+  three.addEventListener('click', makeYellow)
 }
 
 function four () {
